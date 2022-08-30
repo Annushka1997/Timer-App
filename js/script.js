b91ed4529cc9b237b14c9ee925557d808a439548
Annushka1997 <annushkach1997@mail.ru>
2022-08-31 01:25:38 +0400

    Timer_App

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const div = document.getElementById("div");
+const text = document.querySelector("h2");
+document.addEventListener("DOMContentLoaded", () => {
+    const time = setInterval (function (month=11,day=31) {
+    const start = new Date ();
+    const end = new Date (2022,month,day,23,59,59);
+    const result = end - start;
+    const days = Math.floor(result/1000/60/60/24);
+	const hours = Math.floor(result/1000/60/60)%24;
+	const minutes = Math.floor(result/1000/60)%60;
+	const seconds = Math.floor(result/1000)%60;
+    text.textContent = `${days} : ${hours} : ${minutes} : ${seconds}`;
+    if (result <=0) {
+        clearInterval(time);
+        div.classList.add("div_active");
+        document.querySelector(".canvas").classList.add("canvas_active");
+    }
+},1000);
+    time();
+});
+
+
+
