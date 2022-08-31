d2c8fcfac86158e701639e099eb426bb2eb6bf09
Annushka1997 <annushkach1997@mail.ru>
2022-08-31 10:12:33 +0400

    Timer___App

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const start = new Date ();
     const end = new Date (2022,month,day,23,59,59);
     const result = end - start;
+    const seconds = Math.floor(result/1000)%60;
+    const minutes = Math.floor(result/1000/60)%60;
+    const hours = Math.floor(result/1000/60/60)%24;
     const days = Math.floor(result/1000/60/60/24);
-	const hours = Math.floor(result/1000/60/60)%24;
-	const minutes = Math.floor(result/1000/60)%60;
-	const seconds = Math.floor(result/1000)%60;
     text.textContent = `${days} : ${hours} : ${minutes} : ${seconds}`;
     if (result <=0) {
         clearInterval(time);
